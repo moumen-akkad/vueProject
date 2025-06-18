0d2326dd05d3e23b6909a60c0cd7d3d0804cd122
Akkad <m_akkad23@cs.uni-kl.de>
2025-06-18 18:02:39 +0200

    first structure draft

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import CompanyIdentification from "../views/CompanyIdentification.vue";
+import NotFound from "../views/NotFound.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+export default createRouter({
+  history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: "/company/id",
+      name: "companyId",
+      component: CompanyIdentification
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound
+    }
 
-export default router
+  ]
+});
